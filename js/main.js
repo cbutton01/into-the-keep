deab345ebf9b2f787035eaf01b21477537012366
cb <csbutton01@hotmail.com>
2018-09-10 12:40:10 -0700

    made commit in wrong repo, moving to new repo

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
+var canvas = document.getElementById("game");
+var canvasContext = canvas.getContext('2d');
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+var fps = 1000/30;
+var map = [
+    0,0,0,0,0,
+]
 
-
-
-function test(){
-    return alert('test!');
+window.onload = function(){
+    setInterval(fps, update());
     
 }
 
-window.onload = function(){
-    var canvas = document.getElementById("game");
-    var canvasContext = canvas.getContext('2d');
-    test();
-    console.log(canvasContext);
+function update(){
+    drawRect(0,0, canvasWidth, canvasHeight, 'black');
+    drawRect(canvasWidth/2, canvasHeight/2, 10, 15, 'white');
+}
+
+function drawRect(startX, startY, width, height, color ) {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(startX, startY, width, height);
 }
