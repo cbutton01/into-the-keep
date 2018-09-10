68333e23d8cacb28d32425d38f75c839ed6e6d2f
cb <csbutton01@hotmail.com>
2018-09-10 11:20:02 -0700

    try and sort out errors

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+
+
+
 function test(){
     return alert('test!');
+    
 }
 
 window.onload = function(){
+    var canvas = document.getElementById("game");
+    var canvasContext = canvas.getContext('2d');
     test();
+    console.log(canvasContext);
 }
