0d6ae91147cd74c05ba9a4fcdea14fd3247489e7
cb <csbutton01@hotmail.com>
2018-10-01 15:02:12 -0700

    sprite will stop as if on ground if it hits a red brick

--- js/player.js
+++ js/player.js
@@ -11,7 +11,29 @@ class Player {
                 canvasContext.drawImage(PLAYER_PIC, this.x, this.y);
             }
         }
+
+            this.checkGround = function(){ 
+                for(var y = 0; y < mapH; y++)
+            {
+            for(var x = 0; x < mapW; x++)
+            {
+                switch(gameMap[((y*mapW)+x)])
+                {
+                    case 1:
+                        if(this.y >= (y*tileH) - this.height && this.x == (x*tileW)){
+                        onGround = true;
+                        }
+                        break;
+                    default:
+                        null
+                    }
+                }
+            }
+        }
+
         this.move = function(){
+
+            this.checkGround();
 
             if(moveLeft && this.x + this.speedX > 0){
                 this.speedX = -3;
@@ -21,14 +43,12 @@ class Player {
                 this.speedX = 0;
             }
             
-            // if(this.y + this.height < canvasHeight && !onGround){
-            //     this.speedY = 10;
-            //     onGround = false;
-            // } else if(this.y + this.height >= canvasHeight){
-            //     this.speedY = 0;
-            //     this.y = canvasHeight - this.height;
-            //     onGround = true;
-            // }
+            if(this.y + this.height < canvasHeight && !onGround){
+                this.speedY = 10;
+                onGround = false;
+            } else if(onGround){
+                this.speedY = 0;
+            }
 
             // if(this.y <= canvasHeight - 150){
             //     jump = false;
@@ -39,7 +59,9 @@ class Player {
                 onGround = false;
             }
 
-            this.speedY += gravity;
+            if(!onGround){
+                this.speedY += gravity;
+            }
             this.x += this.speedX;
             this.y += this.speedY;            
         }
