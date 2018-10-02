b0193786e53f3b5bf21a1c8a230b973a8ea3c5d7
cb <csbutton01@hotmail.com>
2018-10-02 10:18:44 -0700

    implement collisions

--- js/map.js
+++ js/map.js
@@ -1,37 +1,137 @@
-let gameMap = [
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1
-];
+world = {
+    map: [
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        5,5,5,5,5,5,5,5,5,5,5,4,0,0,2,5,5,5,5,5,
+        1,1,1,1,1,1,1,1,1,1,1,1,5,5,1,1,1,1,1,1
+    ],
+    columns:20,
+    rows:15,
+    tile_size:40,
 
-var tileW = 40, tileH = 40;
-var mapW = 20, mapH = 15;
+    //collision is placed inside the world object because the tile numbers are directly related to how the collisions are detcted
+    collision: {
+
+        1:function(object, row, column) {
+
+        if (this.topCollision(object, row)) { return; }// if no top collision
+        this.rightCollision(object, column);           // try right side collision
+
+        },
+
+        // the 2 tile type has a top and a left side to collide with
+        2:function(object, row, column) {
+
+        if (this.topCollision(object, row)) { return; }
+        this.leftCollision(object, column);
+
+        },
+
+        // the 3 tile type only blocks movement through the right side
+        3:function(object, row, column) {
+
+        this.rightCollision(object, column);
+
+        },
+
+        // the 4 tile type has collision on all sides but the bottom
+        4:function(object, row, column) {
+
+        if (this.topCollision(object, row)) { return; }// you only want to do one
+        if (this.leftCollision(object, column)) { return; }// of these collision
+        this.rightCollision(object, column);// responses. that's why there are if statements
+
+        },
+
+        // the 5 tile only does collision if the object falls through the top
+        5:function(object, row, column) {
+
+        this.topCollision(object, row);
+
+        },
+        leftCollision(object, column) {
+
+        if (object.velocity_x > 0) {// If the object is moving right
+
+            var left = column * world.tile_size;// calculate the left side of the collision tile
+
+            if (object.x + object.width * 0.5 > left && object.old_x <= left) {// If the object was to the right of the collision object, but now is to the left of it
+
+            object.velocity_x = 0;// Stop moving
+            object.x = object.old_x = left - object.width * 0.5 - 0.001;
+
+            return true;
+
+            }
+
+        }
+
+        return false;
+
+        },
+
+        rightCollision(object, column) {
+
+        if (object.velocity_x < 0) {
+
+            var right = (column + 1) * world.tile_size;
+
+            if (object.x + object.width * 0.5 < right && object.old_x + object.width * 0.5 >= right) {
+
+            object.velocity_x = 0;
+            object.old_x = object.x = right - object.width * 0.5;
+
+            return true;
+
+            }
+
+        }
+
+        return false;
+
+        },
+
+        topCollision(object, row) {
+
+        if (object.velocity_y > 0) {
+
+            var top = row * world.tile_size;
+
+            if (object.y + object.height > top && object.old_y + object.height <= top) {
+
+            object.jumping = false;
+            object.velocity_y = 0;
+            object.old_y = object.y = top - object.height - 0.01;
+
+            return true;
+
+            }
+
+        }
+
+        return false;
+
+        }
+
+    }
+}
 
 function drawMap(){
-for(var y = 0; y < mapH; y++)
-	{
-		for(var x = 0; x < mapW; x++)
-		{
-            switch(gameMap[((y*mapW)+x)])
-			{
-				case 1:
-                    drawRect(x*tileW,y*tileH, tileW, tileH, 'red')
-					break;
-                default:
-                    null
-			}
-        }
-    }
+    for (let index = world.map.length - 1; index > -1; -- index) {
+
+        canvasContext.fillStyle = (world.map[index] > 0)?("#0099" + world.map[index] + "f"):"#303840";
+        canvasContext.fillRect((index % world.columns) * world.tile_size, Math.floor(index / world.columns) * world.tile_size, world.tile_size, world.tile_size);
+
+      }
 }
--- js/player.js
+++ js/player.js
@@ -19,7 +19,7 @@ function drawPlayer() {
 
 function movePlayer() {
     if (controller.up && player.jumping == false) {
-        player.velocity_y -= 20;
+        player.velocity_y -= 25;
         player.jumping = true;
     }
 
@@ -31,9 +31,58 @@ function movePlayer() {
         player.velocity_x += 0.5;
     }
 
-    //player.velocity_y += 1.5;
+    player.velocity_y += 1.5;
     player.x += player.velocity_x;
     player.y += player.velocity_y;
     player.velocity_x *= 0.9;
     player.velocity_y *= 0.9;
+
+    if (player.x < 0) {
+
+        player.velocity_x = 0;
+        player.old_x = player.x = 0;
+
+      } else if (player.x + player.width > canvasWidth) {
+
+        player.velocity_x = 0;
+        player.old_x = player.x = canvasWidth - player.width;
+
+      }
+
+      if (player.y < 0) {
+
+        player.velocity_y = 0;
+        player.old_y = player.y = 0;
+
+      } else if (player.y + player.height > canvasHeight) {
+
+        player.velocity_y = 0;
+        player.old_y = player.y = canvasHeight - player.height;
+
+      }
+
+      // calculate the player's x and y tile position in the tile map
+      var tile_x = Math.floor((player.x + player.width * 0.5) / world.tile_size);
+      var tile_y = Math.floor((player.y + player.height) / world.tile_size);
+      // get the value at the tile position in the map
+      var value_at_index = world.map[tile_y * world.columns + tile_x];
+
+      // if it's not an empty tile, we need to do narrow phase collision detection and possibly response!
+      if (value_at_index != 0) {
+
+        // simply call one of the routing functions in the collision object and pass
+        // in values for the collision tile's location in grid/map space
+        world.collision[value_at_index](player, tile_y, tile_x);
+
+      }
+
+      tile_x = Math.floor((player.x + player.width * 0.5) / world.tile_size);
+      tile_y = Math.floor((player.y + player.height) / world.tile_size);
+      value_at_index = world.map[tile_y * world.columns + tile_x];
+
+      if (value_at_index != 0) {
+
+        world.collision[value_at_index](player, tile_y, tile_x);
+
+      }
 }
